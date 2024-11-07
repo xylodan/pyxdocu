4134e0bb790a6f79ca39875b49259ff9542c2d57
Dan Paul <dan@xylollc.com>
2024-11-07 11:25:38 -0800

    updated pics for sparklabs article

--- .docusaurus/client-modules.js
+++ .docusaurus/client-modules.js
@@ -3,4 +3,5 @@ export default [
   require("C:\\Users\\dan\\OneDrive\\Xylo\\websites\\pyxdocu\\pyxdocu\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
   require("C:\\Users\\dan\\OneDrive\\Xylo\\websites\\pyxdocu\\pyxdocu\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
   require("C:\\Users\\dan\\OneDrive\\Xylo\\websites\\pyxdocu\\pyxdocu\\src\\css\\custom.css"),
+  require("C:\\Users\\dan\\OneDrive\\Xylo\\websites\\pyxdocu\\pyxdocu\\node_modules\\@docusaurus\\plugin-google-gtag\\lib\\gtag"),
 ];
