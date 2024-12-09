1559b511724aa12b0bf807e2d5edbc4501b3d528
Dan Paul <dan@xylollc.com>
2024-12-08 20:14:17 -0800

    fix broken site

--- .docusaurus/routes.js
+++ .docusaurus/routes.js
@@ -3,43 +3,8 @@ import ComponentCreator from '@docusaurus/ComponentCreator';
 
 export default [
   {
-    path: '/__docusaurus/debug',
-    component: ComponentCreator('/__docusaurus/debug', '5ff'),
-    exact: true
-  },
-  {
-    path: '/__docusaurus/debug/config',
-    component: ComponentCreator('/__docusaurus/debug/config', '5ba'),
-    exact: true
-  },
-  {
-    path: '/__docusaurus/debug/content',
-    component: ComponentCreator('/__docusaurus/debug/content', 'a2b'),
-    exact: true
-  },
-  {
-    path: '/__docusaurus/debug/globalData',
-    component: ComponentCreator('/__docusaurus/debug/globalData', 'c3c'),
-    exact: true
-  },
-  {
-    path: '/__docusaurus/debug/metadata',
-    component: ComponentCreator('/__docusaurus/debug/metadata', '156'),
-    exact: true
-  },
-  {
-    path: '/__docusaurus/debug/registry',
-    component: ComponentCreator('/__docusaurus/debug/registry', '88c'),
-    exact: true
-  },
-  {
-    path: '/__docusaurus/debug/routes',
-    component: ComponentCreator('/__docusaurus/debug/routes', '000'),
-    exact: true
-  },
-  {
     path: '/blog',
-    component: ComponentCreator('/blog', '407'),
+    component: ComponentCreator('/blog', '61b'),
     exact: true
   },
   {
@@ -109,7 +74,7 @@ export default [
   },
   {
     path: '/blog/tags/pyx',
-    component: ComponentCreator('/blog/tags/pyx', '043'),
+    component: ComponentCreator('/blog/tags/pyx', '600'),
     exact: true
   },
   {
@@ -119,12 +84,12 @@ export default [
   },
   {
     path: '/blog/tags/uncefact',
-    component: ComponentCreator('/blog/tags/uncefact', '34b'),
+    component: ComponentCreator('/blog/tags/uncefact', 'ebc'),
     exact: true
   },
   {
     path: '/blog/tags/untp',
-    component: ComponentCreator('/blog/tags/untp', '7ae'),
+    component: ComponentCreator('/blog/tags/untp', 'e84'),
     exact: true
   },
   {
